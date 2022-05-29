207a2b92d7eb64771ee3e705a9ebafac75029590
lfedosova <valger.l@mail.ru>
2022-05-29 18:56:33 +0300

    added second test

---
+++ qaguru_13_1_git/src/test/java/FistTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AssertionsKt;
+import org.junit.jupiter.api.Test;
+
+public class FistTest {
+
+
+@Test
+void firstTest() {
+    Assertions.assertTrue( 3>1);
+}
+}
+
+@Test
+void secondTest() {
+    Assertions.assertTrue( 3>2);
+}
+
